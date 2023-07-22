ae29cd9abd65f0f4266e1f418331c35508f9d7be
lenaruch <89196825388@mail.ru>
2023-07-23 01:05:50 +0300

    problem on generating tree

--- src/family_tree/Human.java
+++ src/family_tree/Human.java
@@ -1,9 +1,9 @@
 package family_tree;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 public class Human {
     private long id;
@@ -43,4 +43,156 @@ public class Human {
         this(name, gender, birthDay, null, father, mother);
     }
 
+    // Метод для добавления ребенка
+    public boolean addChild(Human child){
+        if (!children.contains(child)){
+            children.add(child);
+            return true;
+        }
+        return false;
+    }
+
+    // Метод для добавления родителя
+    public boolean addParent(Human parent){
+        if (!children.contains(parent)){
+            children.add(parent);
+            return true;
+        }
+        return false;
+    }
+
+    // Метод получения отца
+    public Human getFather(){
+        for (Human parent: parents){
+            if (parent.getGender() == Gender.Male){
+                return parent;
+            }
+        }
+        return null;
+    }
+
+
+    // Метод получения матери
+    public Human getMother(){
+        for (Human parent: parents){
+            if (parent.getGender() == Gender.Female){
+                return parent;
+            }
+        }
+        return null;
+    }
+
+    public int getAge(){
+        if (deathDay == null){
+            return getPeriod(birthDay, LocalDate.now());
+        } else {
+            return (getPeriod(birthDay, deathDay));
+        }
+    }
+
+    // Метод для вычисления возраста
+    private int getPeriod(LocalDate birthDay, LocalDate deathDay) {
+        Period diff = Period.between(birthDay,deathDay);
+        return diff.getYears();
+    }
+
+    // Метод получения пола
+    public Gender getGender(){return gender;}
+
+    public String getName(){return name;}
+    public long getId(){return id;}
+    public void setId(long id){this.id = id;}
+    public void setSpouse(Human spouse){this.spouse=spouse;}
+    public Human getSpouse(){return spouse;}
+    public LocalDate getBirthDay(){return birthDay;}
+    public LocalDate getDeathDay(){return deathDay;}
+    public List<Human> getParents() {return parents;}
+    public List<Human> getChildren()  {return children;}
+    public void setBirthDay(LocalDate birthDay){this.birthDay=birthDay;}
+    public void setDeathDay(LocalDate deathDay){this.deathDay=deathDay;}
+
+    @Override
+    public String toString() {
+        return getInfo();
+    }
+
+    private String getInfo() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("id:");
+        sb.append(id);
+        sb.append(", имя:");
+        sb.append(name);
+        sb.append(", пол:");
+        sb.append(gender);
+        sb.append(", возраст:");
+        sb.append(getAge());
+        sb.append(", ");
+        sb.append(getSpouseInfo());
+        sb.append(", ");
+        sb.append(getFatherInfo());
+        sb.append(", ");
+        sb.append(getMotherInfo());
+        sb.append(", ");
+        sb.append(getChildrenInfo());
+        return sb.toString();
+    }
+
+    private String getSpouseInfo() {
+        String res = "супруг(а): ";
+        if (spouse == null){
+            res += "нет";
+        } else {
+            res += spouse.getName();
+        }
+        return res;
+    }
+
+    private String getFatherInfo() {
+        String res = "отец: ";
+        Human father = getFather();
+        if (father != null){
+            res += father.getName();
+        } else {
+            res += "неизвестно";
+        }
+        return res;
+    }
+
+    private String getMotherInfo() {
+        String res = "мать: ";
+        Human mother = getMother();
+        if (mother != null){
+            res += mother.getName();
+        } else {
+            res += "неизвестно";
+        }
+        return res;
+    }
+
+    private String getChildrenInfo() {
+        StringBuilder res = new StringBuilder();
+        res.append("дети: ");
+        if (children.size() != 0){
+            res.append(children.get(0).getName());
+            for (int i = 1; i < children.size(); i++) {
+                res.append(", ");
+                res.append(children.get(i).getName());
+            }
+        } else {
+            res.append("отсутсвуют");
+        }
+        return res.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this==obj){
+            return true;
+        }
+        if (!(obj instanceof Human)){
+            return false;
+        }
+        Human human = (Human) obj;
+        return human.getId() == getId();
+    }
 }
--- src/family_tree/Main.java
+++ src/family_tree/Main.java
@@ -1,7 +1,33 @@
 package family_tree;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-
+        FamilyTree tree = testTree();
+        System.out.println(tree);
     }
+
+    static FamilyTree testTree(){
+        FamilyTree tree = new FamilyTree();
+
+        Human valera = new Human("Валера", Gender.Male, LocalDate.of(1977,4,12));
+        tree.add(valera);
+        Human elena = new Human("Елена", Gender.Female, LocalDate.of(1968,8,27));
+        tree.add(elena);
+
+        tree.setWedding(valera.getId(), elena.getId());
+
+        Human egor = new Human("Егор", Gender.Male, LocalDate.of(1994,8,2),valera,elena);
+        tree.add(egor);
+        Human alena = new Human("Алена", Gender.Female, LocalDate.of(1998,1,10),valera, elena);
+        tree.add(alena);
+
+        Human anfisa = new Human("", Gender.Female, LocalDate.of(1945,7,19));
+        anfisa.addChild(valera);
+        tree.add(anfisa);
+
+        return tree;
+    }
+
 }
